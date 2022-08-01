48a59fd7ada0ffbd80e1590b8634238ee322124a
hillova <83608015+hillova@users.noreply.github.com>
2022-08-01 18:36:59 +0200

    cleaned js code

--- index.js
+++ index.js
@@ -22,25 +22,24 @@ function changeData(response) {
   let tempElement = document.querySelector(".temperature");
   let temperature = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
-
-  tempElement.innerHTML = `${temperature}°C`;
   let windElement = document.querySelector("#wind");
   let wind = response.data.wind.speed;
-  windElement.innerHTML = `${wind} km/h`;
   let humidityElement = document.querySelector("#humidity");
   let humidity = response.data.main.humidity;
-  humidityElement.innerHTML = `${humidity} %`;
   let descriptionElement = document.querySelector(".description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
-
   let iconElement = document.querySelector("#weather-icon");
   let weatherImage = response.data.weather[0].icon;
+  let dateElement = document.querySelector("#last-updated");
+
+  cityElement.innerHTML = response.data.name;
+  tempElement.innerHTML = `${temperature}°C`;
+  windElement.innerHTML = `${wind} km/h`;
+  humidityElement.innerHTML = `${humidity} %`;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${weatherImage}.png`
   );
-  let dateElement = document.querySelector("#last-updated");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
@@ -48,13 +47,13 @@ function showCity(city) {
   let units = "metric";
   let apiKey = "9d6be2d6ae989b89ba01f7b622ef3053";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
   axios.get(apiUrl).then(changeData);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let input = document.querySelector("#city-input");
-  showCity(input.value);
 }
 
 let submitButton = document.querySelector("form");
